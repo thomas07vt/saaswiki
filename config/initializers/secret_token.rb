7713ffe73dbaac1692e84c7406b57690c4778a82
= <thomas07@vt.edu>
2014-05-05 10:48:28 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Saaswiki::Application.config.secret_key_base = '5759411c082c6098c074751af1e08ef127f39e4cb78b68a0e4587820d0c014aadd728209d85d899483f0dfd6bc1c9b763adda9f646566fed714bb51c31385558'
